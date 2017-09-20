5d2261661a71c32a283915a066b14169ee20b224
Nicolas Gotchac <ngotchac@gmail.com>
2017-09-20 18:09:18 +0200

    Implement rate limiting

--- backend/src/routes/utils.js
+++ backend/src/routes/utils.js
@@ -5,6 +5,9 @@
 
 const redis = require('../redis');
 
+// No more than 20 addresses per IP
+const RATE_LIMITED_ADDRESSES = 20;
+
 function error (ctx, code = 400, body = 'Invalid request') {
   ctx.status = code;
   ctx.body = body;
@@ -14,12 +17,24 @@ function getIp () {
 
 }
 
-function rateLimiter (address, ip) {
-  const exceeded = false;
+async function rateLimiter (address, ip) {
+  const key = `picops::rate-limiter::${ip}`;
+  const json = await redis.get(key);
+  const addresses = json
+    ? JSON.parse(json)
+    : [];
 
-  if (exceeded) {
-    throw new Error('you exceeded the number of requests your IP can make');
+  // Address is already there, everything is OK
+  if (addresses.includes(address)) {
+    return;
   }
+
+  if (addresses.length >= RATE_LIMITED_ADDRESSES) {
+    throw new Error('you exceeded the number of addresses your IP can use');
+  }
+
+  addresses.push(address);
+  await redis.set(key, JSON.stringify(addresses));
 }
 
 module.exports = {
