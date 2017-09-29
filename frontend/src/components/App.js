a545597c7763d82dc1e8176ad3df214da737f301
Nicolas Gotchac <ngotchac@gmail.com>
2017-09-29 15:01:32 +0200

    Update UI (#33)
    * Add checkbox to DETAILS
    
    * Remove onfido doc link

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -153,16 +153,27 @@ class MainApp extends Component {
           maxWidth: '600px'
         }}>
           <p style={{ lineHeight: '1.5em' }}>
-            Welcome to the <b>P</b>arity <b>ICO</b> <b>P</b>assport <b>S</b>ervice (PICOPS). PICOPS is a two sided service to Ethereum end users that want to support projects that offer what has come to be known by the name initial coin offerings (ICOs).
+            Welcome to the <b>P</b>arity <b>ICO</b> <b>P</b>assport <b>S</b>ervice (PICOPS).
+            PICOPS is a two sided service to Ethereum end users that want to support projects
+            that offer what has come to be known by the name initial coin offerings (ICOs).
           </p>
           <p style={{ lineHeight: '1.5em' }}>
-            PICOPS offers a means to validate that the owner of an Ethereum wallet has passed an ID background check stating that they are not part of a restricted set of users (e.g. US citizen or individuals on official watchlists). The background check is run via a third party, namely Onfido. Parity has set up a smart contract system to record the outcome of the background check on the public Ethereum blockchain, i.e. whitelist a Ethereum addresses that are owned by non-restricted users.
+            PICOPS offers a means to validate that the owner of an Ethereum wallet has passed
+            an ID background check stating that they are not part of a restricted set of users
+            (e.g. US citizen or individuals on official watchlists). The background check is run
+            via a third party, namely Onfido. Parity has set up a smart contract system to record the outcome of the background check on the public Ethereum blockchain, i.e. whitelist a Ethereum addresses that are owned by non-restricted users.
           </p>
           <p style={{ lineHeight: '1.5em' }}>
-            To use PICOPS as an end user, you will have to make a small upfront payment of Ether. If you do not currently own an Ethereum wallet to be certified and instead store Ether on an exchange, you will have the opportunity  to create a wallet file during the certification  process. Once the fee is paid, you will be asked to provide a scan of a <a href='https://onfido.com/an-applicant-guide.pdf'>document</a> to Onfido, an ID verification service, to verify your identity.
+            To use PICOPS as an end user, you will have to make a small upfront payment of Ether.
+            If you do not currently own an Ethereum wallet to be certified and instead store Ether
+            on an exchange, you will have the opportunity  to create a wallet file during the
+            certification  process. Once the fee is paid, you will be asked to provide a scan
+            of a document to <a href='https://onfido.com/'>Onfido</a>,
+            an ID verification service, to verify your identity.
           </p>
           <p style={{ lineHeight: '1.5em' }}>
-            Processing the payment and verifying your identity document will take a few minutes. Please make sure to not close this window during the verification process.
+            Processing the payment and verifying your identity document will take a few minutes.
+            Please make sure to not close this window during the verification process.
           </p>
         </div>
 
