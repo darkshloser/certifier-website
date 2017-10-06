5ace4e5d47928c75ef9981728c7c75d0368b110d
Nicolas Gotchac <ngotchac@gmail.com>
2017-10-06 17:07:49 +0200

    Update config

--- config/default.js
+++ config/default.js
@@ -5,7 +5,8 @@ module.exports = {
   },
   nodeWs: 'ws://127.0.0.1:8546/',
   certifierContract: '0x06C4AF12D9E3501C173b5D1B9dd9cF6DCC095b98',
-  feeContract: '0xD5d12e7c067Aecb420C94b47d9CaA27912613378',
+  oldFeeContract: '0xD5d12e7c067Aecb420C94b47d9CaA27912613378',
+  feeContract: '0x72cd7c2a62dbD502456f4fe97428FF77c8b1fB1F',
   onfido: {
     token: '',
     webhookToken: '',
